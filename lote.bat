a4b886766f5e9f805bcd30e54a2b26e69a042467
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-11-15 20:38:57 -0500

    se agrego el archivo lote.bat

---
+++ lote.bat
@@ -0,0 +1,231 @@
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples01.py "carlos Garcia" 1998 2019
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples01.py "frank" 1991 2018
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples01.py  "luis" 1987 2014
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples02.py 20 19  4 15
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples02.py 10 7 14 11
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples02.py 18 20 8 10
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples03.py "kelly" "televisor" 800 2
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples03.py  "cesar" "cocina" 400 1
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples03.py  "yenifer" "usb" 20 5
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples04.py  "alexis" 30 35 10
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples04.py  "fernando" 20 10 5
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples04.py  "brayan" 22 35 10
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples05.py  "abraham" "celulares" 400 9
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples05.py  "jhonatan" "tablets" 200 5
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples05.py  "jesus" "chip bitel" 5 20
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples06.py  "Daniel" 50 50
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples06.py  "Albaro" 20 60
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples06.py  "Pablo" 5 30
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples07.py  300 100 2
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples07.py  50 20 4
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples07.py  400 50 2
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples08.py  "David" "cemento" 40 5
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples08.py  "Mario" "yeso" 10 10
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples08.py  "Javier" "pintura" 35 10
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples09.py  80 25 21
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples09.py  2 12 9
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples09.py  7 15 90
+
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples10.py  "Jorge" "celulares" "lavadoras" 300 400 1 1
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples10.py  "Piero" "ubs" "televisores" 20 120 2 1
+python LIZA_DAMIAN_CARLOS/SIMPLES/condicionales_simples10.py  "Leonardo" "biclicletas" "laptos" 300 4000 1 1
+
+
+
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles01.py  "luciano" 1984 2019
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles01.py  "walter" 2004 2019
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles01.py  "Maurizio" 1974 2019
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles02.py  20 19 20 20
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles02.py  10 11  13 5
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles02.py  7 10 12 20
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles03.py  "Lorenzo" "licuadora" 198 1
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles03.py  "Donato" "dispensor de agua" 89 5
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles03.py  "Federico" "maquina de sellado" 100 1
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles04.py  "Adriano" 20 15 20
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles04.py  "Alonzo" 40 30 50
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles04.py  "Fabricio" 20 7 10
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles05.py  "Santiago" "soplete para reposteria" 75 1
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles05.py  "Fernando" "Microondas" 270 5
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles05.py  "Marlo" "cuchillo electrico" 69 4
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles06.py  "Marco" 5 35
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles06.py  "Ariel" 17 55
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles06.py  "frank" 5 25
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles07.py  50 40 2
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles07.py  100 89 4
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles07.py  30 40 2
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles08.py  "Erick" "fierro grueso" 100 1
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles08.py  "Benjamin" "cemento" 55 7
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles08.py  "Thiago" "tubos grueso"  20 5
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles09.py  9 10 2
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles09.py  90 1 21
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles09.py  7 12 1
+
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles10.py  "Simon" "cafetera" "cama" 150 250  1  1
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles10.py  "Gabriel" "licuadora" "refrigeradora" 200 1000  3  1
+python LIZA_DAMIAN_CARLOS/DOBLES/condicionales_dobles10.py  "Matias" "plancha" "lentes de sol"  90  100  1  1
+
+
+
+
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples01.py  "Carlos" 1983 2019
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples01.py  "Ivan" 1989 2019
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples01.py  "Dairon" 2010 2019
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples02.py  20 13 17 15
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples02.py  10 15 5 9
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples02.py  20 20 17 19
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples03.py  "Henry" "celular" 350 1
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples03.py  "Pablo" "televisores" 800 5
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples03.py  "Luis" "USB" 20 5
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples04.py  "Abel" 10 40 35
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples04.py  "Miguel" 40 20 37
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples04.py  "Alberto" 10 10 5
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples05.py  "Nicolas" "chip Movistar" 80 6
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples05.py  "Yamil" "celulares" 800 6
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples05.py  "Sergio" "chip Claro" 10 6
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples06.py  "Javier" 20 40
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples06.py  "Mateo" 30 50
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples06.py  "Daniel" 10 38
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples07.py  50  50  3
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples07.py  150 140 4
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples07.py  10 10 3
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples08.py  "Thiago" "focos"  20 11
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples08.py  "Frank" "cemento" 50  30
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples08.py  "Piero" "lija gruesa" 3 15
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples09.py  300  2   1
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples09.py  150  6   1
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples09.py  1   2   50
+
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples10.py  "Ronaldo" "tostadora" "microondas" 300  250  1  1
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples10.py  "Neymar" "sonido" "refrigeradora" 800 1000  2  1
+python LIZA_DAMIAN_CARLOS/MULTIPLES/condicionales_multiples10.py  "Junior" "USB" "DVD" 20  79  1  1
+
+
+
+
+
+
+
+
+
+
+
+python rojascubas/simples/ejercicio1.py 12 14 15 18
+python rojascubas/simples/ejercicio1.py 07 08 05 02
+python rojascubas/simples/ejercicio1.py 13 11 11.5 14.5
+python rojascubas/simples/ejercicio2.py  "juan" "12345F" "1234567"
+python rojascubas/simples/ejercicio2.py  "Ana"  "hgufrgr" "54514545"
+python rojascubas/simples/ejercicio2.py  "jose" "145127" "5487584"
+python rojascubas/simples/ejercicio3.py  25 23 22
+python rojascubas/simples/ejercicio3.py  23 0.4 -5
+python rojascubas/simples/ejercicio3.py  4 5 7
+python rojascubas/simples/ejercicio4.py  14 13 12
+python rojascubas/simples/ejercicio4.py  2 4 5
+python rojascubas/simples/ejercicio4.py  17 20 8
+python rojascubas/simples/ejercicio5.py  "Raul" "leche" 14.5
+python rojascubas/simples/ejercicio5.py  "Jhonatan" "cereal" 12.0
+python rojascubas/simples/ejercicio5.py  "Daniel" "cafe" 2.50
+python rojascubas/simples/ejercicio6.py  14 "julio" 2014
+python rojascubas/simples/ejercicio6.py  33 "marzo" 2010
+python rojascubas/simples/ejercicio6.py  02 "enero" 2009
+python rojascubas/simples/ejercicio7.py  "carlos" "TOTTUS" 300
+python rojascubas/simples/ejercicio7.py  "maria" "Plaza Vea" 100
+python rojascubas/simples/ejercicio7.py  "eduardo" "Metro" 50
+python rojascubas/simples/ejercicio8.py  100 200 300
+python rojascubas/simples/ejercicio8.py  1 2 3
+python rojascubas/simples/ejercicio8.py  1000 1 0
+python rojascubas/simples/ejercicio9.py  10 20 30
+python rojascubas/simples/ejercicio9.py  -4 3 5
+python rojascubas/simples/ejercicio9.py  9 8 7
+python rojascubas/simples/ejercicio10.py  500 "Rosa"  "SANSUNG" "Telefonica" 98754215
+python rojascubas/simples/ejercicio10.py  100 "Paul"  "LG" "CLARO" 95864122
+python rojascubas/simples/ejercicio10.py  50 "Frank" "NOKIA" "BITEL" 95451211
+python rojascubas/dobles/ejercicio11.py 12 13 14 15
+python rojascubas/dobles/ejercicio11.py 02 03 04 14
+python rojascubas/dobles/ejercicio11.py 20 20 20 20
+python rojascubas/dobles/ejercicio12.py "Rosa" "121" "1234567"
+python rojascubas/dobles/ejercicio12.py "Ana" "12345F" "dfdhf"
+python rojascubas/dobles/ejercicio12.py "martha" "12345F" "1234567"
+python rojascubas/dobles/ejercicio13.py  1 2 3
+python rojascubas/dobles/ejercicio13.py  -1 25  3
+python rojascubas/dobles/ejercicio13.py   1 0 2
+python rojascubas/dobles/ejercicio14.py  14 5 2
+python rojascubas/dobles/ejercicio14.py  0 2 3
+python rojascubas/dobles/ejercicio14.py  20 1 0.5
+python rojascubas/dobles/ejercicio15.py  "juan perez" "Arroz" 14.2
+python rojascubas/dobles/ejercicio15.py  "Pedro" "papa" 6.5
+python rojascubas/dobles/ejercicio15.py  "Eva" "celulares" 0
+python rojascubas/dobles/ejercicio16.py  15 "diciembre" 2014
+python rojascubas/dobles/ejercicio16.py  -15 "abrli" 2011
+python rojascubas/dobles/ejercicio16.py  0 "mayo" 1998
+python rojascubas/dobles/ejercicio17.py "jhon Arnold" "Don pedrito" 400
+python rojascubas/dobles/ejercicio17.py  "josemaria" "Saga Falabella" 100
+python rojascubas/dobles/ejercicio17.py  "Van Dame" "TOTTUS" 50
+python rojascubas/dobles/ejercicio18.py  12 13 14
+python rojascubas/dobles/ejercicio18.py  0.5 0.3 0.4
+python rojascubas/dobles/ejercicio18.py  200 150 100
+python rojascubas/dobles/ejercicio19.py  -5 -2 -4
+python rojascubas/dobles/ejercicio19.py  0 2 3
+python rojascubas/dobles/ejercicio19.py  20 25 30
+python rojascubas/dobles/ejercicio20.py  100 "Rosa ramos" "Xl" "Movistar" 92345745
+python rojascubas/dobles/ejercicio20.py  1000 "Ana" "huawuei" "Metro" 9456781
+python rojascubas/dobles/ejercicio20.py  -200 "Aroon" "Bmobile" "Claro" 945678
+python rojascubas/multiples/ejercicio21.py  02 01 03 05
+python rojascubas/multiples/ejercicio21.py  14 11 11 12
+python rojascubas/multiples/ejercicio21.py  20 20 20 20
+python rojascubas/multiples/ejercicio22.py "jose" "12345F" "1234567"
+python rojascubas/multiples/ejercicio22.py  "sebastian" "120s" "1234567"
+python rojascubas/multiples/ejercicio22.py  "karla" "12345F" "kfrgnj"
+python rojascubas/multiples/ejercicio23.py  10 20 30
+python rojascubas/multiples/ejercicio23.py   2 3 4
+python rojascubas/multiples/ejercicio23.py  100 200 300
+python rojascubas/multiples/ejercicio24.py 0 0 0
+python rojascubas/multiples/ejercicio24.py 14 15 16
+python rojascubas/multiples/ejercicio24.py 20 5 3
+python rojascubas/multiples/ejercicio25.py "juan rosales" "camote" 12.3
+python rojascubas/multiples/ejercicio25.py  "ricardo" "gaseosa" 15.2
+python rojascubas/multiples/ejercicio25.py  "fernando" "menestras" 2.0
+python rojascubas/multiples/ejercicio26.py  0 "febriray" 1492
+python rojascubas/multiples/ejercicio26.py  33 "marzo" 2000
+python rojascubas/multiples/ejercicio26.py  4 "abril" 2019
+python rojascubas/multiples/ejercicio27.py  "jose" "TOTTUS" 500
+python rojascubas/multiples/ejercicio27.py  "FRANCO" "SAGA FALABELLA" 20
+python rojascubas/multiples/ejercicio27.py  "RAUL" "GALLO MAS GALLO" 700
+python rojascubas/multiples/ejercicio28.py  14 15 13
+python rojascubas/multiples/ejercicio28.py  11 11 11
+python rojascubas/multiples/ejercicio28.py  12 14 15
+python rojascubas/multiples/ejercicio29.py  200 333 147
+python rojascubas/multiples/ejercicio29.py  7 1 1
+python rojascubas/multiples/ejercicio29.py  2 2 2
+python rojascubas/multiples/ejercicio30.py  1000 "MARIA" "SAMSUMG" "MOVISTAR" 98765432
+python rojascubas/multiples/ejercicio30.py  500 "JOSE" "LG" "CLARO" 95874612
+python rojascubas/multiples/ejercicio30.py  10 "FERNANDO" "HUAWEI" "BITEL" 95874612
+
+
+
+
